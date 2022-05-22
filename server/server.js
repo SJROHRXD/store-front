20ec933d2ea88c38209635e0b5b513c1330f8b61
Zakk Fast <zakkfastpro@gmail.com>
2022-05-21 20:36:12 -0700

    set up server and DB connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const connectDB = require('./config/connection');
+const path = require('path');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('API running'));
+// Connect DB
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port `));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
