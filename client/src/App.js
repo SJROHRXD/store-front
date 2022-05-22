a486f9670870c5864a774d2504ef3ce7e05a0a5a
SJRXD <rohr.olasz@gmail.com>
2022-05-21 23:00:11 -0700

    cleaned up + overcommented app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/layout/Header';
 // single-quotes is for sociopaths but okay Zakk
@@ -7,8 +6,9 @@ function App() {
   return (
     // <div className="App">
     <Header />
+        // then a container and all the things here ✨
     // </div>
   );
-}
+};
 
 export default App;
