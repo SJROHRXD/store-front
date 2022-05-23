950ecc966854dda515cb1e6632d166ad0ffb2e49
SJRXD <rohr.olasz@gmail.com>
2022-05-22 17:03:28 -0700

    added product card and assets folder

--- client/src/components/layout/ProductModal.jsx
+++ client/src/components/layout/ProductModal.jsx
@@ -1,3 +1,17 @@
 // product name
 // categories, subcategories
 // description
+
+function ProductCard() {
+    return (
+        <div className="productCard">
+            <div className="productCard_body">
+                <img src=""/>
+                <h2 className="productCard_name"></h2>
+                <p className="productCard_description"></p>
+            </div>
+            <button className="viewModal_btn"></button>
+            <button className="viewPage_btn"></button>
+        </div>
+    );
+};
